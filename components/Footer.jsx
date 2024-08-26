af78d27c671471e89715238b561bb5b01deae0f1
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2024-08-27 00:26:05 +0300

    add

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,13 +6,10 @@ export default function Footer() {
     <div id="contact !z-50">
       <div className="w-[90%] mx-auto !mt-[150px] ">
         <div className=" w-full relative pb-4 bg-gradient-to-br from-[#080852 to-[#1956DC border-4 border-green-500  bg-[#080852] bg-center rounded-2xl">
-
           <img
             className="absolute   bottom-[3px] md:bottom-[-5%] left-[9%] md:left-[23%] !w-[80%]  md:!w-[60%]   -z-2 opacity-[0.2"
             src="/right-left.png"
           />
-
-
 
           <div className="grid relative z-10 py-12 grid-cols-1 lg:grid-cols-2 items-center  lg:items-center">
             <div className="px-12 pt-4 h-full">
@@ -20,7 +17,10 @@ export default function Footer() {
                 Ready to become
               </h3>
               <div className="flex items-center">
-                <img className=" w-[36px] h-[25px] md:w-[85px] md:h-[55px]" src="/UN.png" />
+                <img
+                  className=" w-[36px] h-[25px] md:w-[85px] md:h-[55px]"
+                  src="/UN.png"
+                />
                 <span className="font-bold text-white text-[30px]  md:text-[75px] uppercase">
                   stoppable?
                 </span>
@@ -40,7 +40,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="pt-32 pb-14 sm:pb-8">
-          <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+          <div className="flex mb-14 md:mb-24 flex-col sm:flex-row sm:items-center sm:justify-between">
             <p className="sm:flex hidde  text-[34px]  items-center font-normal  text-[#191919]  xl:text-[65px]">
               Unlock Your
               <div className="flex items-center mx-2">
@@ -48,58 +48,58 @@ export default function Footer() {
                   className="xl:w-[72px] object-fit xl:h-[46px] w-[24px] h-[24px] md:ml-2 md:-mb-2"
                   src="/UN.png"
                 />
-                <span className="text-[#191919]  uppercase font-semibold">lmtd</span>   <span classNameName="mx-2">Potential</span>
+                <span className="text-[#191919]  uppercase font-semibold">
+                  lmtd
+                </span>{" "}
+                <span classNameName="mx-2">Potential</span>
               </div>
-              {/* Potential */}
             </p>
-            {/* <img className="sm:hidden w-[90%]" src="/assets/icon-ft.png" /> */}
-            <button onClick={()=>{window.scrollTo(0, 0);}} className="bg-white self-end mt-4 p-4 rounded-full text-[#080852] shadow-lg text-lg flex items-center justify-center w-10 h-10">
+
+            <button
+              onClick={() => {
+                window.scrollTo(0, 0);
+              }}
+              className="bg-white self-end mt-4 p-4 rounded-full text-[#080852] shadow-lg text-lg flex items-center justify-center w-10 h-10"
+            >
               ↑
             </button>
           </div>
-          <div className="flex flex-col-rever sm:flex-row flex-col justify-between">
-            <div className="flex flex-col gap-3">
+
+          <div className="">
+            <div className="flex  flex-col-rever flex-col md:flex-row justify-between w-full   gap-3">
               <p className="sm:text-[40px] mt-2 sm:mt-0 text-[20px]">
                 hello@weareunlmtd.com
               </p>
-              <span className="text-[18px] font-thin">
-                2024 © weareunlmtd. All rights reserved
-              </span>
-            </div>
-            <div className="flex  flex-col-rever flex-col   gap-3">
+
               <div className="flex gap-8  sm:text-[40px]">
                 <div>LinkedIn</div>
                 <div>Instagram</div>
               </div>
+            </div>
 
-              <span className="text-[18px]  font-thin sm:text-right">
-                <Link classNameName=" !no-underline" href={'/privacy'}>
+            <div className="flex mt-12 flex-col-rever flex-col md:flex-row justify-between w-full   gap-3">
+              <p className="text-[18px] mt-2 sm:mt-0 ">
+                2024 © weareunlmtd. All rights reserved
+              </p>
 
- Privacy
-                </Link>
-               <span classNameName="px-2">|</span>
-                <Link classNameName="!no-underline" href={'/cookies'}>
+              <div className="flex gap-8  stext-[18px]">
+                <div>
+                  <Link classNameName=" !no-underline" href={"/privacy"}>
+                    Privacy
+                  </Link>
+                </div>
+                <div>
+                  <Link classNameName=" !no-underline" href={"/cookies"}>
+                    Cookie Disclaimer
+                  </Link>
+                </div>
 
-
-  Cookie Disclaimer
-</Link>
-
-
-<span classNameName="px-2 ">|</span>
-
-
-
-
-<Link classNameName=" !no-underline" href={'/privacy'}>
-
-Terms and conditions
-               </Link>
-
-
-
-
-
-              </span>
+                <div>
+                  <Link classNameName=" !no-underline" href={"/terms"}>
+                    Terms and conditions
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
